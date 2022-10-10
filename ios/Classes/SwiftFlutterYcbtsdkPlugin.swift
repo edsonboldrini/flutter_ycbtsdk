43e93c704f4de8f93d3138595d7cdba9dc09b24b
Edson Boldrini <edsonboldrini@gmail.com>
2022-10-10 15:15:33 -0300

    Adding iOS permissions

--- ios/Classes/SwiftFlutterYcbtsdkPlugin.swift
+++ ios/Classes/SwiftFlutterYcbtsdkPlugin.swift
@@ -1,6 +1,8 @@
 import Flutter
 import UIKit
 import YCProductSDK
+import CoreBluetooth
+import SwiftyJSON
 
 public var devicesList = [CBPeripheral]()
 
@@ -28,3 +30,47 @@ public class SwiftFlutterYcbtsdkPlugin: NSObject, FlutterPlugin {
     }
   }
 }
+
+class SwiftStreamHandler: NSObject, FlutterStreamHandler {
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        if arguments as! String == "scan"{
+            
+            YCProduct.scanningDevice { devices, error in
+                var scanBLEResponse : [ScanBLEResponse] = [ScanBLEResponse]()
+                devicesList = devices
+                for device in devices {
+                    print(device.name ?? "", device.macAddress)
+                    let BLEObject : ScanBLEResponse = ScanBLEResponse(name: device.name ?? "", mac: device.macAddress, rssi: device.rssiValue)
+                    scanBLEResponse.append(BLEObject)
+                    
+                }
+                do {
+                    let jsonData = try JSONEncoder().encode(scanBLEResponse)
+                    let jsonString = String(data: jsonData, encoding: .utf8)!
+                    events(jsonString)
+                } catch {
+                    print(error)
+                }
+                //                let jsonData = self.json(from: scanBLEResponse)
+                //                events(jsonData)
+            }   
+        }
+        //        events(FlutterError(code: "ERROR_CODE",
+        //                             message: "Detailed message",
+        //                             details: nil)) // in case of errors
+        //        events(FlutterEndOfEventStream) // when stream is over
+        return nil
+    }
+    
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        return nil
+    }
+    
+    func json(from object:Any) -> String? {
+        guard let data = try? JSONSerialization.data(withJSONObject: object, options: []) else {
+            return nil
+        }
+        return String(data: data, encoding: String.Encoding.utf8)
+    }
+}
+
