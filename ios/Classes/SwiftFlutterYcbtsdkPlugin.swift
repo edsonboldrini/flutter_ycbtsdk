937d12421da500c48cad0a5fd9a3feb48bed50fc
Edson Boldrini <edsonboldrini@gmail.com>
2022-10-10 19:41:10 -0300

    Improve identation

--- ios/Classes/SwiftFlutterYcbtsdkPlugin.swift
+++ ios/Classes/SwiftFlutterYcbtsdkPlugin.swift
@@ -21,56 +21,74 @@ public class SwiftFlutterYcbtsdkPlugin: NSObject, FlutterPlugin {
     _ = YCProduct.shared
   }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {    
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
     case "getPlatformVersion":
-        result("iOS " + UIDevice.current.systemVersion)
+			result("iOS " + UIDevice.current.systemVersion)
+			break
+    case "startScan":
+			let scanTimeout: Double = call.arguments as! Double
+			YCProduct.scanningDevice(delayTime: scanTimeout) { devices, error in
+				for device in devices {
+					var scanBLEResponse : [ScanBLEResponse] = [ScanBLEResponse]()
+					print("name: \(device.name ?? ""); mac: \(device.macAddress); rssi: \(device.rssiValue)")
+					let BLEObject : ScanBLEResponse = ScanBLEResponse(name: device.name ?? "", mac: device.macAddress, rssi: device.rssiValue)
+					scanBLEResponse.append(BLEObject)
+					do {
+						let jsonData = try JSONEncoder().encode(scanBLEResponse)
+						let jsonString = String(data: jsonData, encoding: .utf8)!
+						// self.methodChannel.invokeMethod("nativeCallSomeFlutterMethod", arguments: nil)
+					} catch {
+						print(error)
+					}
+				}
+			}
+			result(nil)
+			break
     default:
-        result("Not implemented method")
+			print("Method not implemented")
+			result(nil)
+			break
     }
   }
 }
 
 class SwiftStreamHandler: NSObject, FlutterStreamHandler {
-    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
-        if arguments as! String == "scan"{
-            
-            YCProduct.scanningDevice { devices, error in
-                var scanBLEResponse : [ScanBLEResponse] = [ScanBLEResponse]()
-                devicesList = devices
-                for device in devices {
-                    print(device.name ?? "", device.macAddress)
-                    let BLEObject : ScanBLEResponse = ScanBLEResponse(name: device.name ?? "", mac: device.macAddress, rssi: device.rssiValue)
-                    scanBLEResponse.append(BLEObject)
-                    
-                }
-                do {
-                    let jsonData = try JSONEncoder().encode(scanBLEResponse)
-                    let jsonString = String(data: jsonData, encoding: .utf8)!
-                    events(jsonString)
-                } catch {
-                    print(error)
-                }
-                //                let jsonData = self.json(from: scanBLEResponse)
-                //                events(jsonData)
-            }   
-        }
-        //        events(FlutterError(code: "ERROR_CODE",
-        //                             message: "Detailed message",
-        //                             details: nil)) // in case of errors
-        //        events(FlutterEndOfEventStream) // when stream is over
-        return nil
-    }
+	public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+		if arguments as! String == "startScan"{
+			YCProduct.scanningDevice { devices, error in
+			var scanBLEResponse : [ScanBLEResponse] = [ScanBLEResponse]()
+			devicesList = devices
+				for device in devices {
+					print(device.name ?? "", device.macAddress)
+					let BLEObject : ScanBLEResponse = ScanBLEResponse(name: device.name ?? "", mac: device.macAddress, rssi: device.rssiValue)
+					scanBLEResponse.append(BLEObject)
+				}
+				do {
+					let jsonData = try JSONEncoder().encode(scanBLEResponse)
+					let jsonString = String(data: jsonData, encoding: .utf8)!
+					events(jsonString)
+				} catch {
+					print(error)
+				}
+				// let jsonData = self.json(from: scanBLEResponse)
+				// events(jsonData)
+			}
+		}
+		// events(FlutterError(code: "ERROR_CODE", message: "Detailed message", details: nil)) // in case of errors
+		// events(FlutterEndOfEventStream) // when stream is over
+		return nil
+	}
     
-    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
-        return nil
-    }
-    
-    func json(from object:Any) -> String? {
-        guard let data = try? JSONSerialization.data(withJSONObject: object, options: []) else {
-            return nil
-        }
-        return String(data: data, encoding: String.Encoding.utf8)
-    }
+	public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+		return nil
+	}
+	
+	func json(from object:Any) -> String? {
+		guard let data = try? JSONSerialization.data(withJSONObject: object, options: []) else {
+				return nil
+		}
+		return String(data: data, encoding: String.Encoding.utf8)
+	}
 }
 
