bea27db407977523c29cd9735f14ce3ace7617be
Edson Boldrini <edsonboldrini@gmail.com>
2022-10-17 23:26:16 -0300

    Adding some ios params

--- ios/Classes/DataResponse.swift
+++ ios/Classes/DataResponse.swift
@@ -6,22 +6,22 @@ class DataResponse : Codable {
     var OOValue: Int?
     var respiratoryRateValue: Int?
     var temperatureValue: Double?
-    var DBPValue: Int?
     var SBPValue: Int?
+    var DBPValue: Int?
     var sportStartTime: Int?
     var sportEndTime: Int?
     var sportStep: Int?
     var sportDistance: Int?
     var sportCalorie: Int?
 
-    init(startTime: Int? = nil, heartValue: Int? = nil, OOValue: Int? = nil, respiratoryRateValue: Int? = nil, temperatureValue: Double? = nil, DBPValue: Int? = nil, SBPValue: Int? = nil, sportStartTime: Int? = nil, sportEndTime: Int? = nil, sportStep: Int? = nil, sportDistance: Int? = nil, sportCalorie: Int? = nil) {
+    init(startTime: Int? = nil, heartValue: Int? = nil, OOValue: Int? = nil, respiratoryRateValue: Int? = nil, temperatureValue: Double? = nil, SBPValue: Int? = nil, DBPValue: Int? = nil, sportStartTime: Int? = nil, sportEndTime: Int? = nil, sportStep: Int? = nil, sportDistance: Int? = nil, sportCalorie: Int? = nil) {
         self.startTime = startTime
         self.heartValue = heartValue
         self.OOValue = OOValue
         self.respiratoryRateValue = respiratoryRateValue
         self.temperatureValue = temperatureValue
-        self.DBPValue = DBPValue
         self.SBPValue = SBPValue
+        self.DBPValue = DBPValue
         self.sportStartTime = sportStartTime
         self.sportEndTime = sportEndTime
         self.sportStep = sportStep
